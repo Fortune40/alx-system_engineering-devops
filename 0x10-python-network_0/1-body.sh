1b5db1b33206009901f1ca535f445219e8b8c01c
Daniel <CJNwagbara@hotmail.com>
2024-07-04 20:51:41 +0100

    Resolve 0x10-python-network_0

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-curl "$1" -sL
+# Get the response body for a given URL for 200 status code responses.
+curl -sL "$1"
